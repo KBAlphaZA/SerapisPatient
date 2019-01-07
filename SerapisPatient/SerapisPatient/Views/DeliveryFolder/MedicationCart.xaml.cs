cda360888d5d8b8442ac91c8130732d325135340
Hacker <Hacker@DESKTOP-271BF02>
2019-01-07 09:51:29 +0200

    added pictures

---
+++ SerapisPatient/SerapisPatient/Views/DeliveryFolder/MedicationCart.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SerapisPatient.Views.DeliveryFolder
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MedicationCart : ContentPage
+	{
+		public MedicationCart ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
