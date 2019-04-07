00c0f78bd2559e71d2892e07bfc36d987a960e16
Khanyisani Buthelezi <khanyisani.buthelezi03@gmail.com>
2019-04-07 15:30:46 +0200

    Added some error logging code.

---
+++ SerapisPatient/SerapisPatient/Helpers/ErrorLogging.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace SerapisPatient.Helpers
+{
+    public static class ErrorLogging
+    {
+        private static string APIAddress = "https://www.addressofApi/";
+
+        //This method sends the error msg to the backend
+        public async static void WriteErrorMsg(DateTime timeAndDateOfLogEntry, object errorMsg, object patientId)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Clear();
+
+                }
+            }
+            catch(Exception Ex)
+            {
+
+            }
+        }
+
+    }
+}
---
+++ SerapisPatient/SerapisPatient/Models/ErrorLoggingModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SerapisPatient.Models
+{
+    public class ErrorLoggingModel
+    {
+        public DateTime TimeOfError { get; set; }
+        public string ErrorMessage { get; set; }
+        public PatientUser PatientId { get; set; }
+
+        public string DeviceType { get; set; }
+    }
+}
