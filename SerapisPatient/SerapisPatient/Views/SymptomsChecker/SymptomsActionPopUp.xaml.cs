c7f59b8049c150b7bd7c8b885932db499e1217de
bonga.ngcobo@kuda.co.za <bonga.ngcobo@kuda.co.za>
2022-04-14 13:36:55 +0200

    added popup

---
+++ SerapisPatient/SerapisPatient/Views/SymptomsChecker/SymptomsActionPopUp.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SerapisPatient.Views.SymptomsChecker
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SymptomsActionPopUp : ContentView
+    {
+        public delegate void ClickExpandDelegate();
+
+        public ClickExpandDelegate OnExpandTapped { get; set; }
+        
+        public SymptomsActionPopUp()
+        {
+            InitializeComponent();
+        }
+        
+        public double FirstSectionHeight => FirstSection.Height;
+
+        private void FirstSection_Tapped(object sender, EventArgs e)
+        {
+            OnExpandTapped?.Invoke();
+        }
+    }
+}
