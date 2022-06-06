71c03d9de90045ca26a3b06117fb649895fb82bc
bonga.ngcobo@kuda.co.za <bonga.ngcobo@kuda.co.za>
2022-06-06 05:36:05 +0200

    Finally working smoothly

---
+++ SerapisPatient/SerapisPatient/ViewModels/MasterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using SerapisPatient.TemplateViews;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.Forms;
+
+namespace SerapisPatient.ViewModels
+{
+    public class MasterViewModel
+    {
+        public MasterViewModel()
+        {
+            DefaultLoadingView popUp = new DefaultLoadingView();
+
+            if (Device.RuntimePlatform == Device.iOS || Device.RuntimePlatform == Device.Android)
+            {
+                popUp.IsLightDismissEnabled = false;
+            }
+
+            if (Device.RuntimePlatform == Device.iOS || Device.RuntimePlatform == Device.Android)
+                App.Current.MainPage.Navigation.ShowPopup(popUp);
+        }
+    }
+}
